b63e83c67bc4334de595d021eabc16d90a842f0d
santi <sdvillal@gmail.com>
2017-08-16 14:34:21 +0200

    ignore build logs

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,8 +16,21 @@ unset SUBDIR
         --enable-version3 \
         --enable-hardcoded-tables \
         --enable-avresample \
-        --enable-libx264 \
-        --enable-libfreetype
+        --enable-libx264
 
 make
 make install
+
+# See also:
+#   https://trac.ffmpeg.org/wiki/CompilationGuide#PerformanceTips
+#   https://www.ffmpeg.org/legal.html
+
+# To enable freetype (drawtext filter) add:
+#   --enable-libfreetype \
+# And add freetype as a build + run dependency on meta.yaml
+# If we do so, we are getting some nasty errors:
+#
+# /opt/rh/devtoolset-2/root/usr/libexec/gcc/x86_64-redhat-linux/4.8.2/ld: warning: libpng16.so.16, needed by /feedstock_root/build_artefacts/ffmpeg_1502289942638/_b_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_place/lib/lib# freetype.so, not found (try using -rpath or -rpath-link)
+# /feedstock_root/build_artefacts/ffmpeg_1502289942638/_b_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_place/lib/libfreetype.so: undefined reference to `png_read_update_info@PNG16_0'
+#...
+#
