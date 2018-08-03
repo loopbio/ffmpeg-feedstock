b2e7cc8ad5a12dea8f59aca894602870e5d423c5
santi <sdvillal@gmail.com>
2018-08-04 01:50:09 +0200

    loopbio changes
    - nvidia and gpl explicit in build string
    - linux only
    - target loopbio channel
    - enable libvpx, x265, aom, nvidia codecs
    - all pins are declared by deps
    - other small comments and improvements

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,26 +3,38 @@
 # unset the SUBDIR variable since it changes the behavior of make here
 unset SUBDIR
 
+# When the new compilers come, we might need this:
+#   --cc="${CC}" \
+# (as seen in the anaconda recipe)
+
 ./configure \
         --prefix="${PREFIX}" \
         --disable-doc \
         --disable-openssl \
         --enable-shared \
         --enable-static \
+        --enable-pic \
+        --enable-pthreads \
         --extra-cflags="-Wall -g -m64 -pipe -O3 -march=x86-64 -fPIC" \
         --extra-cxxflags="-Wall -g -m64 -pipe -O3 -march=x86-64 -fPIC" \
         --extra-libs="-lpthread -lm -lz" \
-        --enable-zlib \
-        --enable-pic \
-        --enable-pthreads \
         --enable-gpl \
         --enable-version3 \
-        --enable-hardcoded-tables \
+        --disable-nonfree \
+        --enable-postproc \
         --enable-avresample \
-        --enable-libfreetype \
+        --enable-hardcoded-tables \
         --enable-gnutls \
+        --enable-zlib \
+        --enable-libfreetype \
         --enable-libx264 \
-        --enable-libopenh264
+        --enable-libopenh264 \
+        --enable-libx265 \
+        --enable-libaom \
+        --enable-libvpx \
+        --enable-cuda \
+        --enable-cuvid \
+        --enable-nvenc
 
 make -j${CPU_COUNT}
 make install -j${CPU_COUNT}
